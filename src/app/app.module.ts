1b59cd805c846c8bed522678c6959cf767f4e7dc
Fynn <fynn.kloepper@gmail.com>
2018-05-28 17:17:21 +0200

    Health-Record

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import {MatFormFieldModule, MatTableModule} from '@angular/material';
+import { MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
+import { HealthRecordComponent } from './health-record/health-record.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HealthRecordComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule
+  ],
+  exports: [
+
   ],
   providers: [],
   bootstrap: [AppComponent]
