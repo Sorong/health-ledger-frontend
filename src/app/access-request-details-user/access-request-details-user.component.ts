eaca5273e2b0cf24d09b177e889f562a7a92b75d
Sorong <patrick_starzynski@web.de>
2018-05-31 13:56:06 +0200

    access-request-details-user

--- src/app/access-request-details-user/access-request-details-user.component.ts
+++ src/app/access-request-details-user/access-request-details-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-access-request-details-user',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./access-request-details-user.component.css']
 })
 export class AccessRequestDetailsUserComponent implements OnInit {
-
+  from = "Dr. Acula"
+  to = "Van Helsing"
+  displayedColumns = ['therapy', 'diagnose', 'recipe', 'incapacity'];
+  ds = new MatTableDataSource(ELEMENT_DATA);
   constructor() { }
+
 
   ngOnInit() {
   }
-
 }
+export class CheckboxAttributes {
+  checked: boolean;
+  indeterminate: boolean;
+  disabled: boolean;
+
+  constructor(initChecked) {
+    this.checked = initChecked;
+    this.indeterminate = false;
+    this.disabled = false;
+  }
+}
+
+export interface Therapy {
+  therapy: string;
+  diagnose: CheckboxAttributes;
+  recipe: CheckboxAttributes;
+  incapacity: CheckboxAttributes;
+}
+
+const ELEMENT_DATA: Therapy[] = [
+  {therapy: 'Behandlung 1', diagnose : new CheckboxAttributes(false), recipe : new CheckboxAttributes(true), incapacity : new CheckboxAttributes(false)},
+  {therapy: 'Behandlung 1', diagnose : new CheckboxAttributes(true), recipe : new CheckboxAttributes(true), incapacity : new CheckboxAttributes(false)},
+  {therapy: 'Behandlung 1', diagnose : new CheckboxAttributes(true), recipe : new CheckboxAttributes(true), incapacity : new CheckboxAttributes(false)}
+];
