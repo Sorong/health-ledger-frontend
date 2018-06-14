4854c37531864e92ec141a897057ce5db918e188
Patrick Starzynski <patrick_starzynski@web.de>
2018-06-14 13:57:00 +0200

    diagnose mock

--- src/app/models/mocks/mock-treatment.ts
+++ src/app/models/mocks/mock-treatment.ts
@@ -1,59 +1,54 @@
-import {Treatment, Category} from '../treatment.model';
+import {Treatment} from '../treatment.model';
 import {PRESCRIPTIONS} from '../mocks/mock-SmartPerscription';
 import {ATTESTATIONS} from '../mocks/mock-attestation';
+import {DIAGNOSES} from './mock-diagnose';
 
 export const TREATMENT: Treatment[] = [
   {
     id: '1',
-    category: Category.bar,
     issue_date: new Date(0),
     patient_name: 'Mr. Nice', doctor_name: 'Dr IQ',
-    diagnose: 'Definitely Aids',
+    diagnose: DIAGNOSES[0],
     prescription: PRESCRIPTIONS[0],
     attestation: null,
   },
   {
     id: '2',
-    category: Category.baz,
     issue_date: new Date(0),
     patient_name: 'foo', doctor_name: 'Dr IQ',
-    diagnose: 'Maybe Adis?',
+    diagnose: DIAGNOSES[1],
     prescription: PRESCRIPTIONS[1],
     attestation: ATTESTATIONS[0],
   },
   {
     id: '3',
-    category: Category.foo,
     issue_date: new Date(0),
     patient_name: 'bar', doctor_name: 'Dr IQ',
-    diagnose: 'Super Aids, sorry',
+    diagnose: DIAGNOSES[2],
     prescription: PRESCRIPTIONS[2],
     attestation: ATTESTATIONS[1],
   },
   {
     id: '4',
-    category: Category.foo,
     issue_date: new Date(0),
     patient_name: 'baz', doctor_name: 'Dr IQ',
-    diagnose: 'AIDS!',
+    diagnose: DIAGNOSES[0],
     prescription: PRESCRIPTIONS[3],
     attestation: ATTESTATIONS[2],
   },
   {
     id: '5',
-    category: Category.foo,
     issue_date: new Date(0),
     patient_name: 'foobar', doctor_name: 'Dr IQ',
-    diagnose: 'WTF is this even?! (spoiler, it\'s Aids)',
+    diagnose: DIAGNOSES[1],
     prescription: PRESCRIPTIONS[4],
     attestation: null,
   },
   {
     id: '6',
-    category: Category.foo,
     issue_date: new Date(0),
     patient_name: 'foobarbaz', doctor_name: 'Dr IQ',
-    diagnose: 'WUT, just a little Aids',
+    diagnose: DIAGNOSES[0],
     prescription: PRESCRIPTIONS[5],
     attestation: ATTESTATIONS[3],
   }
--- src/app/models/treatment.model.ts
+++ src/app/models/treatment.model.ts
@@ -1,19 +1,14 @@
 import {Prescription} from './prescription.model';
 import {Attestation} from '../models/attestation.model';
+import {Diagnose} from './diagnose.model';
 
 export class Treatment {
   id: string;
-  category: Category;
   issue_date: Date;
   patient_name: string;
   doctor_name: string;
-  diagnose: string;
+  diagnose: Diagnose;
   prescription: Prescription;
   attestation: Attestation;
 }
 
-export enum Category {
-  foo = 'Foo',
-  bar = 'bar',
-  baz = 'baz'
-}
