405940d8459867eb846dc167ae249332f9106a3a
Fynn <fynn.kloepper@gmail.com>
2018-06-18 17:17:34 +0200

    QR-Code Implementierung

--- src/app/views/qr-code/qr-code.component.ts
+++ src/app/views/qr-code/qr-code.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user.service';
+import {StateService} from '../../services/state.service';
 
 @Component({
   selector: 'app-qr-code',
@@ -6,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qr-code.component.css']
 })
 export class QrCodeComponent implements OnInit {
-  public publicKey: string = null;
-  constructor() {
-    this.publicKey = 'Hello World';
+  public userData: string = null;
+  constructor(private stateService: StateService) {
+    this.userData = JSON.stringify({
+      'publicKey' : this.stateService.user.publicKey,
+      'name' : this.stateService.user.name
+    });
   }
 
   ngOnInit() {
